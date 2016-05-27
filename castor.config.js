1a8a8df71ab0ef6cd15677b657dbe70b20f96366
Nicolas Lamblin <nicolas.lamblin@outlook.com>
2016-05-27 14:53:21 +0200

    Delete MONGODB_PORT
    Now integrated into starter.js of castor-core.

--- castor.config.js
+++ castor.config.js
@@ -1,11 +1,6 @@
 'use strict'
-// to allow mongodb host and port injection thanks
-// to the MONGODB_PORT environment parameter
-// (docker uses it)
-var mongoHostPort = process.env.MONGODB_PORT ? process.env.MONGODB_PORT.replace('tcp://', '') : 'localhost:27017';
 
 module.exports = {
-  "connectionURI": 'mongodb://' + mongoHostPort + '/ezark',
   "browserifyModules" : [
     "mongodb-querystring",
     "heartbeats",
