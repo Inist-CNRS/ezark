4a3c3dd34a8c9bf30685e579e20e7bec3c1d7f4a
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-09-15 21:58:12 +0200

    don't use proxy with localhost

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,6 @@ if [ "$(cat /app/config.local.json)" == "{}" ]; then
 	cp -f /app/config.sample.json /app/config.local.json
 fi
 
+export no_proxy=localhost,127.0.0.1
+export NO_PROXY=localhost,127.0.0.1
 exec /apt/ezark
